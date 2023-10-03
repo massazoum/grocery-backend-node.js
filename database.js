e6198f02b683f7a48e9a77d8d633b6fb2b532b8c
massa <zoumaniguimassa12@gmail.com>
2023-10-03 19:59:35 +0000

    Update to deploy

--- database.js
+++ database.js
@@ -1,14 +1,12 @@
-import pg from 'pg';
-
-const { Pool } = pg;
-
+const { Pool } = require('pg')
+ 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 })
 
-pool.connect((err)=>{
- if(err)throw err
- console.log("connect to possgreSQL successfully")
+pool.connect((err) => {
+    if (err) throw err
+    console.log("Connect to PostgreSQL successfully!")
 })
 
-module.exports = pool ;
+module.exports = pool
