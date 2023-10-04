238f1e254905428e8d24f6a27d8f23d0ea993053
massa <zoumaniguimassa12@gmail.com>
2023-10-04 07:35:49 +0000

    Update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+require('dotenv').config()
 
 // Middleware
 app.use(bodyParser.json());
